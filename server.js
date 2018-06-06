c90948e6b18678ce357ad06e9b8a5cfbb3fbbb5e
Kumidori <nico.weingaertner@hs-furtwangen.de>
2018-06-07 00:48:39 +0200

    test without cors

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const cors = require('cors');
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const app = express();
-const path = '/graphql'
-app.options('*', cors())
-app.use(cors());
 
 const server = new ApolloServer({ typeDefs, resolvers });
-registerServer({ server, app, path});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
